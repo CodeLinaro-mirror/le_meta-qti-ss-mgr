bf50d2dbc2cbae537df23a14c14b9d00646f68ac
Bulbul Dabi <quic_bdabi@quicinc.com>
2024-08-30 16:15:49 +0530

    csm-logger: Add flag to check for first trigger
    Time sync on actual device takes few seconds.
    And once the time is changed the first run of this service
    will not be able to fetch logs from the start i.e,logs with the
    time before the sync wont be fetched by journald because we are
    using --since option. Added a mechanism to differentiate first
    trigger and subsequent triggers of the timer.
    
    Change-Id: I1cd42e5a1b1fe2116c228be9a99fac5977de0c51
    (cherry picked from commit 7770cb0a5c466f5ff7241af346522055d59e31eb)

--- recipes/csm-logger/files/csm-logger.sh
+++ recipes/csm-logger/files/csm-logger.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
 #SPDX-License-Identifier: BSD-3-Clause-Clear
-journalctl --quiet --since='5 minutes ago' > /data/logs/journal.log
+
+FLAG_FILE="/var/volatile/csm-logger-flag"
+
+if [ ! -f "$FLAG_FILE" ]; then
+    echo "csm-logger triggered first time after bootup" > "$FLAG_FILE"
+    journalctl --quiet > /data/logs/journal.log
+else
+    journalctl --quiet --since='5 minutes ago' > /data/logs/journal.log
+fi
+sync
